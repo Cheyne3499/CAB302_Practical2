3ecaebbe3575e831165330a83e912cccb5d5a869
Cheyne Francis <n10014055@qut.edu.au>
2024-07-30 13:31:46 +1000

    Project initialisation and class definitions

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,85 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    //Static list of users, acting as a database
+    private static ArrayList<User> users = new ArrayList<>();
+
+    private static IAuthenticationService authService = new IAuthenticationService() {
+        @Override
+        public User signUp(String username, String password) {
+            return null;
         }
+
+        @Override
+        public User logIn(String username, String password) {
+            return users.get(0);
+        }
+    };
+
+    private static boolean isRunning = true;
+
+    public static void main(String[] args) {
+        users.add(new User("test", "test"));
+        while (isRunning) {
+            showMenu();
+        }
+    }
+
+    public static void showMenu() {
+        System.out.println("Welcome to the To-Do List Application!");
+        System.out.println("1. Log in");
+        System.out.println("2. Sign up");
+        System.out.println("3. Exit");
+        System.out.print("Enter your choice: ");
+        // Ask for user choice
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        handleMenu(choice);
+    }
+
+    public static void handleMenu(int choice) {
+        switch (choice) {
+            case 1:
+                onLogIn();
+                break;
+            case 2:
+                onSignUp();
+                break;
+            case 3:
+                onExit();
+                break;
+            default:
+                System.out.println("Invalid choice!");
+                showMenu();
+        }
+    }
+
+    public static void onLogIn() {
+        System.out.print("Enter your username: ");
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
+        System.out.print("Enter your password: ");
+        String password = scanner.nextLine();
+        User user = authService.logIn(username, password);
+        System.out.println("Welcome, " + user.getUsername() + "!");
+        // TODO Later: Add the to-do list operations
+    }
+
+    public static void onSignUp() {
+        System.out.print("Enter your username: ");
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
+        System.out.print("Enter your password: ");
+        String password = scanner.nextLine();
+        User user = authService.signUp(username, password);
+        // TODO Later: Shows a message based on the result
+    }
+
+    public static void onExit() {
+        isRunning = false;
     }
 }
